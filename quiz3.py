b5f72d3d6107e3be20de7170c33446d23cdc8491
Jared Christopher <jarchri@siue.edu>
2024-01-27 20:16:48 -0600

    added quiz3.py file

---
+++ quiz3.py
@@ -0,0 +1,11 @@
+#Created by: Jared Christopher
+
+#iterate through a list
+colors = ["red", "green", "yellow", "blue"]
+for x in colors:
+    print(x)
+    
+#iterate through a string
+s = "red"
+for x in s:
+    print(x)
